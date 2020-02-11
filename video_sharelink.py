5cc17d87b2088375e86bd7788e760f63d138d1fb
zhengzh1 <17551018764@163.com>
2020-02-11 18:25:25 +0800

    change video_sharelink

--- video_sharelink.py
+++ video_sharelink.py
@@ -7,6 +7,7 @@ folder_sharelink = [
 
 # track验证视频--俯视folder--视频详情sharelink
 video_sharelink = [
-    'http://pan.zerozero.cn:88/index.php/s/dIo4tddfDGeZ0cM',
-    'http://pan.zerozero.cn:88/index.php/s/BHbUcdbJCZeON9w'
+    'http://pan.zerozero.cn:88/index.php/s/nYRGUNYQjlUtqtb',
+    'http://pan.zerozero.cn:88/index.php/s/uuIMUbDTkGJW6lI',
+    'http://pan.zerozero.cn:88/index.php/s/FFx4kUPi7X9p6RF'
     ]
