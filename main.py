043d1568b1c5eab2f97f5b8bca468e8727091519
zhengzh1 <17551018764@163.com>
2020-02-13 21:19:53 +0800

    fix global variables

--- main.py
+++ main.py
@@ -7,19 +7,20 @@ downloadTar_list = []
 downloadVid_list = []
 
 
-def track_output(number):
+def track_output(number, video_list):
     outVid_count = number
-    print('22222222'+downloadVid_list)
-    for video_name in downloadVid_list:
+    print('222222222')
+    for video_name in video_list:
+        print('3333333', video_name)
         # -t '1' 为track状态
         shell = f"./track_test_ubuntu.out -i './video_mp4/{video_name}' -o './track_output/{outVid_count}_output_video.mp4' -t '1'"
         os.system(shell)
         outVid_count += 1
 
 
-def detect_output(number):
+def detect_output(number, video_list):
     outVid_count = number
-    for video_name in downloadVid_list:
+    for video_name in video_list:
         # -t '0' 为detect状态
         shell = f"./track_test_ubuntu.out -i './video_mp4/{video_name}' -o './detect_output/{outVid_count}_output_video.mp4' -t '0'"
         os.system(shell)
@@ -28,5 +29,5 @@ def detect_output(number):
 
 if __name__ == "__main__":
     downloadVideo_curl.download_tar(initCount, downloadTar_list, downloadVid_list)
-    track_output(initCount)
-    detect_output(initCount)
+    track_output(initCount, downloadVid_list)
+    detect_output(initCount, downloadVid_list)
