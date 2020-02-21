fa5680796a0e5a7a99cb13e23c54c9d75fc58b2d
zhengzh1 <17551018764@163.com>
2020-02-21 19:08:03 +0800

    fix bug: forget to remove out_video last time

--- main.py
+++ main.py
@@ -9,10 +9,8 @@ downloadVid_list = []
 
 def track_output(number, video_list):
     outVid_count = number
-    for i in video_list:
-        print('222222222+'+i)
+    os.system("rm -rf ./track_output/*")
     for video_name in video_list:
-        print('3333333', video_name)
         # -t '1' 为track状态
         shell = f"./track_test_ubuntu.out -i './video_mp4/{video_name}' -o './track_output/{outVid_count}_output_video.mp4' -t '1'"
         os.system(shell)
@@ -21,6 +19,7 @@ def track_output(number, video_list):
 
 def detect_output(number, video_list):
     outVid_count = number
+    os.system("rm -rf ./detect_output/*")
     for video_name in video_list:
         # -t '0' 为detect状态
         shell = f"./track_test_ubuntu.out -i './video_mp4/{video_name}' -o './detect_output/{outVid_count}_output_video.mp4' -t '0'"
