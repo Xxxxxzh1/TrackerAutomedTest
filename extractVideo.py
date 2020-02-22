5bb3f8e5fdb27c57502c9cc5bd0cfe4ea582ad43
zhengzh1 <17551018764@163.com>
2020-02-22 22:57:38 +0800

    fix bug: forget to rm folder-Temp_video

--- extractVideo.py
+++ extractVideo.py
@@ -10,7 +10,7 @@ def getVideo(count, zip_list, video_list):
 
     for zipName in zip_list:
 
-        extractZip_shell = f"unzip ./video_zip/{zipName} -d './video_zip/Temp_video/' && mv './video_zip/Temp_video'/*/* './video_zip/Temp_video/' && rmdir './video_zip/Temp_video/*'"
+        extractZip_shell = f"unzip ./video_zip/{zipName} -d './video_zip/Temp_video' && mv './video_zip/Temp_video'/*/* './video_zip/Temp_video/' && rmdir './video_zip/Temp_video/*'"
         rmDS_shell = f"rm -rf ./video_zip/Temp_video/.DS_Store"
 
         # 提取压缩包次级文件夹下的所有视频至/Temp_video/路径下
@@ -18,7 +18,7 @@ def getVideo(count, zip_list, video_list):
 
         os.system(rmDS_shell)
         # 得到当前压缩包所有视频名称列表
-        videoInZip_list = os.listdir('./video_zip/Temp_video/')
+        videoInZip_list = os.listdir('./video_zip/Temp_video/*.mp4')
         print('video in zip: ', videoInZip_list)
 
         for videoInZip_name in videoInZip_list:
@@ -28,4 +28,5 @@ def getVideo(count, zip_list, video_list):
             os.system(rename_shell)
             video_list.append(f"{vid_count}_video.mp4")
             vid_count += 1
+        os.system('rm -rf ./video_zip/Temp_video')
     print('video_list: ', video_list)
