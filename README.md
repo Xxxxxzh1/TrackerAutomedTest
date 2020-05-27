f5da14dd66d3bc3cae4d92d7b09576cd9cb4a849
zhengzh1 <17551018764@163.com>
2020-05-27 20:13:52 +0800

    update README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+Description
+====
+- 采用可执行文件 .out 对视频文件做track/detect处理
+- 依赖：OS
+- 在Python3环境运行main.py
+
+History
+====
+- 20200210:
+更改 ubuntu18-tracker-autorun 服务器上的执行文件为track_test_ubuntu.out
+
+- 20200211:
+增加对视频文件夹链接的读取--解压--输出视频功能
---
+++ main.py
@@ -0,0 +1,34 @@
+
+import downloadVideo_curl
+import os
+
+initCount = 0
+downloadZip_list = []
+downloadVid_list = []
+
+
+def track_output(number, video_list):
+    outVid_count = number
+    os.system("rm -rf ./track_output/*")
+    for video_name in video_list:
+        # -t '1' 为track状态
+        shell = f"./track_test_ubuntu.out -i './video_mp4/{video_name}' -o './track_output/{outVid_count}_output_video.mp4' -t '1'"
+        os.system(shell)
+        outVid_count += 1
+
+
+def detect_output(number, video_list):
+    outVid_count = number
+    os.system("rm -rf ./detect_output/*")
+    for video_name in video_list:
+        # -t '0' 为detect状态
+        shell = f"./track_test_ubuntu.out -i './video_mp4/{video_name}' -o './detect_output/{outVid_count}_output_video.mp4' -t '0'"
+        os.system(shell)
+        outVid_count += 1
+
+
+if __name__ == "__main__":
+    downloadVideo_curl.download_zip(initCount, downloadZip_list, downloadVid_list)
+    # print('after download vid_list: ', downloadVid_list)
+    track_output(initCount, downloadVid_list)
+    detect_output(initCount, downloadVid_list)
