0ae02cf26790031b45bc9c45efa771e6c58b04f5
zhengzh1 <17551018764@163.com>
2020-05-30 16:45:39 +0800

    link pcTracker repo

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def track_output(number, video_list):
     os.system("rm -rf ./track_output/*")
     for video_name in video_list:
         # -t '1' 为track状态
-        shell = f"./track_example -i './video_mp4/{video_name}' -o './track_output/{outVid_count}_output_video.mp4'"
+        shell = f"../Tracker/build/track_example -i './video_mp4/{video_name}' -o './track_output/{outVid_count}_output_video.mp4'"
         os.system(shell)
         outVid_count += 1
 
@@ -22,7 +22,7 @@ def detect_output(number, video_list):
     os.system("rm -rf ./detect_output/*")
     for video_name in video_list:
         # -t '0' 为detect状态
-        shell = f"./track_example -i './video_mp4/{video_name}' -o './detect_output/{outVid_count}_output_video.mp4'"
+        shell = f"../Tracker/build/track_example -i './video_mp4/{video_name}' -o './detect_output/{outVid_count}_output_video.mp4'"
         os.system(shell)
         outVid_count += 1
 
