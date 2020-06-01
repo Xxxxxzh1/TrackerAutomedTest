35fc33b05fd036d468a1ed204a6722f30b272682
zhengzh1 <17551018764@163.com>
2020-06-01 16:10:30 +0800

    add repo_sync shell

---
+++ repo_sync.sh
@@ -0,0 +1,7 @@
+#!/usr/bin
+
+cd /home/zz/workspace/workspace/Tracker
+repo sync
+cd /home/zz/workspace/workspace/Tracker/build
+make
+echo "++++++++++++++++++ repo sync success! ++++++++++++++++++"
