ec3c8a5358331e16fc89cc980ff8639a2db0d127
zhengzh1 <17551018764@163.com>
2020-06-02 17:37:07 +0800

    update input video_link

--- video_sharelink.py
+++ video_sharelink.py
@@ -8,9 +8,7 @@ folder_sharelink = [
 
 # track验证视频--俯视folder--视频详情sharelink
 video_sharelink = [
-    'http://pan.zerozero.cn:88/index.php/s/rBAtjU5aHs7Ot3s',
-    'http://pan.zerozero.cn:88/index.php/s/4bw3pbkij1E28s5',
-    'http://pan.zerozero.cn:88/index.php/s/trKGiuyP7p8tDBS',
-    'http://pan.zerozero.cn:88/index.php/s/5JzxbxFmiYUNKek',
-    'http://pan.zerozero.cn:88/index.php/s/76oW35GozVm6Gcc'
+    'http://pan.zerozero.cn:88/index.php/s/vuhJpbsJAbD0XaZ',
+    'http://pan.zerozero.cn:88/index.php/s/71ke8taWwxZPfpP',
+    'http://pan.zerozero.cn:88/index.php/s/UPy14yvEWXats6U'
     ]
